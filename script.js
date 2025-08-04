b275ff089490ef602b4e00419590d43a6a65b44f
DombSTG <viniciusdombidau@gmail.com>
2025-08-04 03:51:56 -0300

    Primeiros passos

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+funcion tmb() {
+    var nome = document.getElementById('nome');
+    var idade = document.getElementById('idade');
+    var altura = document.getElementById('altura');
+    var peso = document.getElementById('peso');
+    var sexo = document.getElementById('sexo');
+    var res1 = document.getElementById('res1');
+
+    if (nome.value.length <== 0 || idade.value.length <== 0 || altura.value.length <== 0 || peso.value.length <== 0 || sexo.value.length <== 0) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!');
+    } else if ( sexo.value == 'Masculino') {
+        var resultado = ((10 * Number(peso.value)) + (6.25 * Number(altura.value)) - (5 * Number(idade.value)) + 5);
+        res1.innerHTML = `Olá <strong>${nome.value}</strong>, seu TMB é: <strong>${resultado.toFixed(2)}</strong> kcal`;
+    } else 
+        var resultado = ((10 * Number(peso.value)) + (6.25 * Number(altura.value)) - (5 * Number(idade.value)) - 161);
+        res1.innerHTML = `Olá <strong>${nome.value}</strong>, seu TMB`
+}
+
+
 function calcular() {
     var prot = document.getElementById('txtproteinas');
     var carb = document.getElementById('txtcarbo');
     var gord = document.getElementById('txtg');
-    var res = document.getElementById('res');
+    var res = document.getElementById('res1');
 
     if (prot.value.length == 0 || carb.value.length == 0 || gord.value.length == 0) {
         window.alert('[ERRO] Verifique os dados e tente novamente!');
