f8bf0e1999d838ae39dec479990119cdc3554e85
DombSTG <viniciusdombidau@gmail.com>
2025-08-04 17:33:00 -0300

    Adição do TDEE

--- script.js
+++ script.js
@@ -49,8 +49,61 @@ function tmb() {
         resultado = ((10 * Number(peso.value)) + (6.25 * alturaNum) - (5 * Number(idade.value)) - 161);
     }
         res1.innerHTML = `Olá <strong>${nome.value}</strong>, seu TMB é: <strong>${resultado.toFixed(2)}</strong> kcal`;
-}
+    document.getElementById('gastoCalorico').style.display = 'block';
+    document.getElementById('calc1').style.display = 'none';
 
+    return resultado;
+
+    }
+
+function calcularTDEE() {
+    const frequencia = document.getElementById('freq');
+    const tipo = document.getElementById('tipo');
+    const duracao = Number(document.getElementById('duracao').value);
+    const objetivo = document.getElementById('objetivo');
+    const peso = parseFloat(document.getElementById('peso').value);
+    const res3 = document.getElementById('res3');
+    let taxa = tmb(); // Chama a função tmb para obter o TMB
+
+    if (frequencia.value.length <= 0 || 
+        tipo.value.length <= 0 || 
+        duracao <= 0 || 
+        objetivo.value.length <= 0) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!');
+        res3.innerHTML = 'Resultado:';
+        return;
+    } 
+    let met;
+    switch (tipo.value) {
+        case 'Musculação':
+            met = 6.0;
+            break;
+        case 'Corrida':
+            met = 8.3;
+            break
+        case 'Caminhada':
+            met = 3.5;
+            break;
+        default:
+            met = 1.0; // Valor de Repouso
+    }
+
+    let caloriasAtividade = (met * peso * duracao) / 7; // Calcular calorias gasta na atividade por dia
+    let tdee = taxa + caloriasAtividade; // Calcular TDEE
+
+    if (objetivo.value === 'Perder Peso') {
+        tdee -= 500; // Reduzir 500 kcal para perda de peso
+    } else if (objetivo.value === 'Ganhar Massa') {
+        tdee += 500; // Aumentar 500 kcal para ganho de massa
+    }   
+    
+    res3.innerHTML = `Com base em seu objetivo: <strong>${tdee.toFixed(2)}</strong> kcal`;
+    
+
+
+
+
+}
 
 // Função do total de Calorias da Dieta
 function calcular() {
@@ -69,7 +122,4 @@ function calcular() {
         var resultado = quatro + (g * 9);
         res.innerHTML = `O total de calorias é: <strong>${resultado}</strong> kcal`;
     }
-
-
-
 }
